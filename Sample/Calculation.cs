7b3605dc8a59cb0d97f96362a965391a6fce48cf
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-31 10:31:52 +0900

    add:足し算を行うメソッドを追加.

--- Sample/Calculation.cs
+++ Sample/Calculation.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sample
+﻿namespace Sample
 {
     public class Calculation
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }

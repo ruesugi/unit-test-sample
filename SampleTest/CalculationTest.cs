314815f8aa402504fda842879283c447fcaee01b
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-31 16:03:51 +0900

    add:失敗するテストを追加した.

--- SampleTest/CalculationTest.cs
+++ SampleTest/CalculationTest.cs
@@ -12,5 +12,12 @@ namespace SampleTest
             Calculation calculation = new Calculation();
             Assert.AreEqual(3, calculation.Add(1, 2));
         }
+
+        [TestMethod]
+        public void 失敗するテスト()
+        {
+            Calculation calculation = new Calculation();
+            Assert.AreEqual(0, calculation.Add(1, 2));
+        }
     }
 }
